94d33e080fc60795eb7015b42145bcf30bae9f40
Manuel <manuelinfosec@gmail.com>
2025-08-09 21:23:27 +0100

    processor: starting to process the  instruction

--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,27 @@
 // program-specific errors
+use std::fmt;
+
+use solana_program::program_error::ProgramError;
+
+#[derive(Debug)]
+pub enum EscrowError {
+    /// Invalid Instruction
+    InvalidInstruction,
+}
+
+// Implement Display for EscrowError
+impl fmt::Display for EscrowError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            EscrowError::InvalidInstruction => write!(f, "Invalid Instruction"),
+        }
+    }
+}
+
+impl std::error::Error for EscrowError {}
+
+impl From<EscrowError> for ProgramError {
+    fn from(e: EscrowError) -> Self {
+        ProgramError::Custom(e as u32)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,4 @@ pub mod entrypoint;
 pub mod state;
 pub mod processor;
 pub mod error;
+pub mod instruction;
